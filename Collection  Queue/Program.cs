8fdbdf63204a355c843d4f460d56035134523e52
Akshaysaini5381 <akshaysaini5381@gmail.com>
2022-07-20 01:42:21 +0530

    Add project files.

---
+++ Collection  Queue/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+namespace Collection__Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue obj = new Queue() ;
+            obj.Enqueue(12);
+            obj.Enqueue("akshay");
+            obj.Enqueue(true);
+            obj.Enqueue(12.790);
+
+            foreach (var item in obj)
+            {
+                Console.WriteLine("Value : "+item);
+            }
+            Console.WriteLine(obj.Count);
+            Console.WriteLine(obj.Peek());
+            
+            
+
+        }
+    }
+}
